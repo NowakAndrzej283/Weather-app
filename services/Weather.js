6ddd0c40b20aca7698c35de41c08623da9ca52fd
Andrzej Nowak <nowakandrzej283@gmail.com>
2026-03-03 14:24:42 +0100

    modify fetchWeather function

--- services/Weather.js
+++ services/Weather.js
@@ -1,64 +1,28 @@
-// importing openmeteo
-import {fetchWeatherApi} from 'openmeteo';
+export const fetchWeather = async () => {
+  let response;
 
-
-// export const fetchWeather = async () => {
+  // Warsaw Coordinates
   const params = {
-    latitude: 52.52,
-    longitude: 13.41,
+    latitude: 52.22,
+    longitude: 21.01,
     hourly: 'temperature_2m'
   };
-  
-  const url = 'https://api.open-meteo.com/v1/forecast';
-  const responses = fetchWeatherApi(url, params);
-  
-  const response = responses[0];
-  
-  const latitude = response.latitude();
-  const longitude = response.longitude();
-  const elevation = response.elevation();
-  const utcOffsetSeconds = response.utcOffsetSeconds();
-  
-  console.log(
-      `\nCoordinates: ${latitude}N ${longitude}E`,
-      `\nElevation: ${elevation}m asl`,
-      `\nTimezone difference to GMT+0: ${utcOffsetSeconds}`
-  );
-  
-  const hourly = response.hourly();
-  
-  const weatherData = {
-    time: Array.from(
-      {
-        length:
-        (Number(hourly.timeEnd()) - Number(hourly.time()))/
-        hourly.interval()
-      },
-      (_, i) => 
-        new Date(
-          (Number(hourly.time()) + i * hourly.interval() + utcOffsetSeconds) * 1000
-      ),
-    )};
 
-    const temperature =  hourly.variables(0).valuesArray();
-    //const weatherCode = hourly.variables(1).valuesArray();
-    
-    export function fetchWeather(){
-        return {
-          time: weatherData,
-          temperature,
-          //weatherCode
-        };
-    }
+  const url = `https://api.open-meteo.com/v1/forecast?latitude=${params.latitude}&longitude=${params.longitude}&hourly=temperature_2m,weather_code`;;
+
+  response = await fetch(url);
+
+  if(!response || response.length === 0){
+    throw new Error('No weather data returned');
+  }
+
+  console.log('RESPONSES', response);
+  const data = await response.json();
+
+  console.log('weather data : ', data);
   
-
-
-  // return {
-  //   time: weatherData,
-  //   temperature,
-  //   //weatherCode
-  // };
-// };
+  return data;
+};
 
 
 
