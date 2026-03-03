1835e2673fadf86c3af47ff8db5e3d1301937bc3
Andrzej Nowak <nowakandrzej283@gmail.com>
2026-03-03 14:24:00 +0100

    modify homePage

--- App.js
+++ App.js
@@ -1,17 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Pressable } from 'react-native';
+import { useState } from 'react';
 
 import { fetchWeather } from './services/Weather';
+import WeatherCard from './components/WeatherCard';
 
 export default function App() {
-  const handleCheckWeather = ()=> {
-    fetchWeather();
+  const [weather, setWeather] = useState([]);
+  const [isDisabled, setIsDisabled] = useState(false);
+  const [data, setData] = useState([]);
+
+  const handleCheckWeather = async()=> {
+    const data = await fetchWeather();
+    console.log(data);
+
+    const formattedData = data.hourly.time.map((time, index)=> ({
+      id: index.toString(),
+      time,
+      temperature: data.hourly.temperature_2m[index],
+      weatherCode: data.hourly.weather_code[index]
+    }));
+    setWeather(formattedData);
+    setIsDisabled(true);
+    setData(formattedData);
+    
+
   };
 
 
   return (
-    <View style={styles.maincontainer}>
-        <Pressable style={styles.button} onPress={handleCheckWeather}>
+    <View style={styles.mainContainer}>
+        <Pressable  style={({pressed}) => 
+          pressed ? styles.pressed : styles.card
+        }
+        onPress={handleCheckWeather}
+        >
+          <WeatherCard data={data}/>
+        </Pressable>
+
+
+        <Pressable style={({pressed}) => 
+           pressed && styles.pressed
+        } 
+          onPress={handleCheckWeather} disabled={isDisabled}>
           <Text>Tap to check the app.</Text>
         </Pressable>
       
@@ -22,10 +53,8 @@ export default function App() {
 const styles = StyleSheet.create({
   mainContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 10
+    backgroundColor: '#4dbcc4',
+    padding: 10,
   },
   button: {
     backgroundColor: 'yellow',
@@ -33,5 +62,14 @@ const styles = StyleSheet.create({
     padding: 10,
     borderRadius: 10,
     margin: 10
+  },
+  pressed: {
+    opacity: 0.75,
+
+  },
+  info: {
+    flex: 1,
+    backgroundColor: "black"
   }
+
 });
